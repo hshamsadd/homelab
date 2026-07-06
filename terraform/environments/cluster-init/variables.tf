004100f20b4c09d92fac0e013d1756ce21186c3f
hushamsadd <hushamsadd@gmail.com>
2026-07-06 18:16:17 +0200

    first commit

---
+++ terraform/environments/cluster-init/variables.tf
@@ -0,0 +1,11 @@
+variable "kube_api_endpoint" {
+  type        = string
+  description = "The Tailscale internal IP address for the K3s API control plane control layer."
+  default     = "https://100.76.59.49:6443"
+}
+
+variable "environments" {
+  type        = list(string)
+  description = "Target deployment namespaces to initialize inside the cluster mesh."
+  default     = ["staging", "production", "gitops-system", "monitoring"]
+}
