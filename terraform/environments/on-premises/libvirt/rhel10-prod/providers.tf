1478357460dd5f09349524836e8fdd777fccca15
hushamsadd <hushamsadd@gmail.com>
2026-07-16 04:12:15 +0200

    feat: add resource requests and limits to cnpg cluster to satisfy compute guardrails

--- terraform/environments/on-premises/libvirt/rhel10-prod/providers.tf
+++ terraform/environments/on-premises/libvirt/rhel10-prod/providers.tf
@@ -1,26 +1,8 @@
-terraform {
-  required_providers {
-    libvirt = {
-      source = "dmacvicar/libvirt"
-      version = "0.9.8"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.5.0"
-    }
-  }
-}
-
 provider "libvirt" {
   uri = "qemu+ssh://${var.libvirt_user}@${var.libvirt_host}/system?sshauth=privkey&keyfile=${var.ssh_private_key_path}&known_hosts_verify=ignore"
 }
 
 provider "tls" {}
 provider "local" {}
-
 
 #uri =  "qemu+ssh://zshamsadd@100.76.59.49/system?sshauth=privkey&keyfile=/home/zshamsadd/.ssh/github_actions_libvirt_key&known_hosts_verify=ignore"
