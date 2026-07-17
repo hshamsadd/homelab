3a6d9e8171c7ada422e3525bc65dc7ad5f558762
hushamsadd <hushamsadd@gmail.com>
2026-07-17 03:17:14 +0200

    security: add scoped Vault policies and Tailscale provisioner

--- terraform/environments/on-premises/libvirt/rhel10-prod/versions.tf
+++ terraform/environments/on-premises/libvirt/rhel10-prod/versions.tf
@@ -1,18 +1,18 @@
 terraform {
   required_version = "~> 1.15.0"
-  
+
   required_providers {
     libvirt = {
-      source = "dmacvicar/libvirt"
+      source  = "dmacvicar/libvirt"
       version = "0.9.8"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.5.0"
-    }
+    # tls = {
+    #   source  = "hashicorp/tls"
+    #   version = "~> 4.0.0"
+    # }
+    # local = {
+    #   source  = "hashicorp/local"
+    #   version = "~> 2.5.0"
+    # }
   }
 }
